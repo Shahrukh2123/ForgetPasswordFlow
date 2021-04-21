f86a11a963e7e40dd48be5c4a8da314cd02186fa
Shahrukh2123 <sh0450230@gmail.com>
2021-04-21 16:50:36 +0530

    Add a space change

--- ForgetPasswordFlow1/Models/EmailParameters.cs
+++ ForgetPasswordFlow1/Models/EmailParameters.cs
@@ -9,7 +9,6 @@ namespace ForgetPasswordFlow1.Models
 {
     public class EmailParameters
     {
-     
         public string To { get; set; }
         public string Subject { get; set; }
         public string Body { get; set; }
