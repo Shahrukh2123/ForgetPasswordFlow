7bd729ac68aeac8c3e9dd716c9dc8700b06792c7
Shahrukh2123 <sh0450230@gmail.com>
2021-04-21 16:42:03 +0530

    Make new changes

--- ForgetPasswordFlow1/Controllers/ForgetPasswordController.cs
+++ ForgetPasswordFlow1/Controllers/ForgetPasswordController.cs
@@ -20,7 +20,7 @@ namespace ForgetPasswordFlow.Controllers
         [HttpGet]
         public ActionResult ForgetPassword()
         {
-            ViewBag.Message = "ForgetPassword,Click  to Recover your Account";
+            ViewBag.Message = "Forget Password ,Flow";
             return View();
         }
      
